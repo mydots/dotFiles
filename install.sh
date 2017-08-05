f44de28d17706fe6966824b508ee89b6fac5e907
Franky W <frankywahl@users.noreply.github.com>
2017-08-05 12:36:33 -0700

    Make installations as functions

--- install.sh
+++ install.sh
@@ -18,56 +18,60 @@ if [[ "$EMAIL_0" == "" ]]; then
 fi
 source ~/.keys
 
-if [[ $KERNEL = "Darwin" ]]; then
-  INSTALL_CMD="brew install"
-fi
-if [[ $KERNEL = "Linux" ]]; then
-  INSTALL_CMD="sudo apt-get install"
-fi
-
-configs=$(ruby -ryaml -e 'data = YAML.load_file("programs.yml"); puts(data["configs"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
-for config in $configs; do
-  if [[ ! -d ${PATH_TO_FILE}/${config}  ]]; then
-    git clone git@github.com:mydots/${config}.git ${PATH_TO_FILE}/${config}
-    # Append to .gitignore if first time
-    if [[ `grep ${config} ${PATH_TO_FILE}/.gitignore | wc -l | awk '{print $1}'` -lt 1 ]]; then
-      echo "${config}" >> ${PATH_TO_FILE}/.gitignore
+install_configs() { 
+  configs=$(PATH_TO_FILE=${PATH_TO_FILE} ruby -ryaml -e 'data = YAML.load_file(File.join(ENV["PATH_TO_FILE"], "programs.yml")); puts(data["configs"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
+  for config in $configs; do
+    if [[ ! -d ${PATH_TO_FILE}/${config}  ]]; then
+      git clone git@github.com:mydots/${config}.git ${PATH_TO_FILE}/${config}
+      # Append to .gitignore if first time
+      if [[ `grep ${config} ${PATH_TO_FILE}/.gitignore | wc -l | awk '{print $1}'` -lt 1 ]]; then
+        echo "${config}" >> ${PATH_TO_FILE}/.gitignore
+      fi
+    else
+      cd ${PATH_TO_FILE}/${config}
+      git pull
     fi
-  else
-    cd ${PATH_TO_FILE}/${config}
-    git pull
-  fi
-done
-git submodule init && git submodule update
-for config in $configs; do
-  if [[ -e ${PATH_TO_FILE}/${config}/install.sh  ]]; then
-    bash ${PATH_TO_FILE}/${config}/install.sh
-  fi
-done
+  done
+  git submodule init && git submodule update
+  for config in $configs; do
+    if [[ -e ${PATH_TO_FILE}/${config}/install.sh  ]]; then
+      bash ${PATH_TO_FILE}/${config}/install.sh
+    fi
+  done
+}
 
-cd ${PATH_TO_FILE}
-apps=$(ruby -ryaml -e 'data = YAML.load_file("programs.yml"); puts(data["apps"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
-echo "The program will install the following:${GREEN}"
-for app in $apps; do
-  echo ${app}
-done
+install_apps() { 
+  cmd=${1}
+  apps=$(PATH_TO_FILE=${PATH_TO_FILE} ruby -ryaml -e 'data = YAML.load_file(File.join(ENV["PATH_TO_FILE"], "programs.yml")); puts(data["apps"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
+  echo "The program will install the following:${GREEN}"
+  for app in $apps; do
+    echo ${app}
+  done
 
-# Make sure User know which programs they are installing and confirm
-echo "${RED}Is the correct list above correct? [y/n]${DEFAULT}"
-read CORRECT
-while [[ "$CORRECT" != "y" && "$CORRECT" != "n" ]]
-do
-  echo "Please enter y or n, is the list above correct?"
+  # Make sure User know which programs they are installing and confirm
+  echo "${RED}Is the correct list above correct? [y/n]${DEFAULT}"
   read CORRECT
-done
-if [ "$CORRECT" = "y" ]; then
-  echo "Ok, installing programs"
-  # Finally, install the programs
-  ${INSTALL_CMD} ${apps}
-fi
+  while [[ "$CORRECT" != "y" && "$CORRECT" != "n" ]]
+  do
+    echo "Please enter y or n, is the list above correct?"
+    read CORRECT
+  done
+  if [ "$CORRECT" = "y" ]; then
+    echo "Ok, installing programs"
+    # Finally, install the programs
+    ${cmd} ${apps}
+  fi
+}
 
-cd ${PATH_TO_FILE}
-if [[ $KERNEL = "Darwin" ]]; then
-  progs=$(ruby -ryaml -e 'data = YAML.load_file("programs.yml"); puts(data["brew"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
+install_brew_cask_apps() { 
+  progs=$(PATH_TO_FILE=${PATH_TO_FILE} ruby -ryaml -e 'data = YAML.load_file(File.join(ENV["PATH_TO_FILE"], "programs.yml")); puts(data["brew"][ARGV[0].downcase].flatten.sort.join(" "))' $KERNEL)
   brew cask install $progs
+}
+
+install_configs
+if [[ $KERNEL = "Linux" ]]; then
+  install_apps "sudo apt-get install"
+elif [[ $KERNEL = "Darwin" ]]; then
+  install_apps "brew install"
+  install_brew_cask_apps
 fi
